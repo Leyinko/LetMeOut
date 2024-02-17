c2ea715f1c5f112e401c8b81d0bad7a4326a8cea
JCBalabuch <balabuchj@gmail.com>
2024-02-16 23:51:56 -0400

    Listeners MenuField

--- src/components/molecules/menuField/MenuField.js
+++ src/components/molecules/menuField/MenuField.js
@@ -1,23 +1,47 @@
-import { playSound } from '../../atoms/audio/Audio';
+import { Lobby } from '../../../pages/Lobby/Lobby';
+import Audio, { playSound } from '../../atoms/audio/Audio';
 import Menu from '../../atoms/menu/Menu';
+import createModal from '../../atoms/modal/Modal';
+import modalContent from '../../atoms/modalContent/ModalContent';
 import './MenuField.css';
 
-const hoverMenu = '../../../../src/assets/audio/sounds/main/Menu-hover.mp3';
+const hoverMenu = Audio('../../../../src/assets/audio/sounds/main/Menu-hover.mp3');
+const instructionsMenu = ['Esto es una prueba para el modal del menu', 'a ver si funciona correctamente'];
+const creditsMenu = ['Manu', 'Luca', 'Jeni'];
 
 const menuField = (parent) => {
-  const menuField = document.createElement('div');
-  menuField.id = 'menu-field';
+  const menuField$ = document.createElement('div');
+  menuField$.id = 'menu-field';
 
-  Menu('START', 'start-button', menuField);
-  Menu('INSTRUCTIONS', 'instructions-button', menuField);
-  Menu('CREDITS', 'credits-button', menuField);
+  Menu('START', 'start-menu', menuField);
+  Menu('INSTRUCTIONS', 'instructions-menu', menuField);
+  Menu('CREDITS', 'credits-menu', menuField);
 
-  const menuItems = document.querySelectorAll('menu-field');
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener('mouseenter', playSound(hoverMenu));
-  });
+  parent.append(menuField$);
 
-  parent.append(menuField);
+  // let menuIntro = document.querySelectorAll('[role="menu-select"]');
+
+  // menuIntro.forEach((element) => {
+  //   element.addEventListener('mouseenter', () => {
+  //     playSound(hoverMenu);
+  //   });
+
+  //   if (element.classList.contains('start-menu')) {
+  //     element.addEventListener('click', () => {
+  //       Lobby();
+  //     });
+  //   } else if (element.classList.contains('instructions-menu')) {
+  //     element.addEventListener('click', () => {
+  //       createModal('instructionsMenu', parent);
+  //       modalContent('instructionsMenuContent', instructionsMenu);
+  //     });
+  //   } else if (element.classList.contains('credits-menu')) {
+  //     element.addEventListener('click', () => {
+  //       createModal('creditsMenu', parent);
+  //       modalContent('creditsMenuContent', creditsMenu);
+  //     });
+  //   }
+  // });
 };
 
 export default menuField;
