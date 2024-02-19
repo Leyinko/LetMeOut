59796d6b3822de68ad01a34105db78601f18156d
leyinko <co.giro.luca@gmail.com>
2024-02-19 15:39:01 +0100

    Lobby Front Alpha 1.0

---
+++ src/pages/lobby/utils/Chat/Chat.js
@@ -0,0 +1,56 @@
+import sendRequest from '../../../../webSocket/webSocket';
+import './Chat.css';
+
+const ChatBox = (username, parent) => {
+  let container = document.querySelector('#chat-container');
+  if (!container) {
+    const chatContainer = document.createElement('div');
+    chatContainer.id = 'chat-container';
+
+    const messages = document.createElement('ul');
+    messages.className = 'messages';
+
+    const inputMessage = document.createElement('input');
+    inputMessage.id = 'message-content';
+    inputMessage.type = 'text';
+
+    inputMessage.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' && e.target.value && e.target.value.trim()) {
+        sendRequest('chatMessage', username, null, null, inputMessage.value);
+        inputMessage.value = '';
+      }
+    });
+
+    chatContainer.append(messages, inputMessage);
+    parent.appendChild(chatContainer);
+
+    chatContainer.classList.toggle('open');
+  } else {
+    container.classList.toggle('open');
+  }
+};
+
+export function chatMessage(user, textContent) {
+  let localUser = document.querySelector('.player').textContent;
+  let messages = document.querySelector('.messages');
+
+  let message = document.createElement('li');
+  message.id = 'message';
+
+  let sender = document.createElement('h4');
+  sender.textContent = user.toUpperCase();
+
+  let content = document.createElement('span');
+  content.textContent = textContent;
+
+  localUser === user.toUpperCase() && (message.className = 'self');
+
+  message.append(sender, content);
+
+  if (messages) {
+    messages.appendChild(message);
+    messages.scrollTop = messages.scrollHeight;
+  }
+}
+
+export default ChatBox;
--- src/pages/lobby/utils/Hub/Hub.js
+++ src/pages/lobby/utils/Hub/Hub.js
@@ -2,6 +2,7 @@ import { copyText } from '../../../../../utils';
 import Button from '../../../../components/atoms/button/Button';
 import Menu from '../../../../components/atoms/menu/Menu';
 import sendRequest, { ws } from '../../../../webSocket/webSocket';
+import ChatBox, { chatMessage } from '../Chat/Chat';
 import './Hub.css';
 
 const PlayersHub = (code, username, party) => {
@@ -10,70 +11,11 @@ const PlayersHub = (code, username, party) => {
 
   const playersHub = document.createElement('div');
   playersHub.id = 'players-hub';
-
   main.insertAdjacentElement('afterbegin', playersHub);
 
   const room = document.createElement('span');
   room.className = 'room-code';
   room.textContent = code;
-
-  // ! TEST CHAT
-
-  const chat = document.createElement('img');
-  chat.src = 'src/assets/images/icons/menu/chat.svg';
-  chat.className = 'chat-code';
-
-  chat.addEventListener('click', (e) => {
-    e.target.classList.toggle('on');
-
-    const chatContainer = document.createElement('div');
-    chatContainer.id = 'chat-container';
-
-    const messages = document.createElement('ul');
-    messages.className = 'messages';
-
-    const inputMessage = document.createElement('input');
-    inputMessage.id = 'chat-test-value';
-    inputMessage.type = 'text';
-
-    inputMessage.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        let message = document.querySelector('#chat-test-value');
-        sendRequest('chatMessage', username, null, null, message.value);
-        message.value = '';
-      }
-    });
-
-    ws.onmessage = function (event) {
-      let data = JSON.parse(event.data);
-      if (data.tag === 'chat') {
-        let littleContainer = document.createElement('li');
-        littleContainer.id = 'little-container';
-
-        let sender = document.createElement('h4');
-        sender.textContent = data.name;
-
-        let content = document.createElement('span');
-        content.textContent = data.message;
-
-        littleContainer.append(sender, content);
-
-        messages.appendChild(littleContainer);
-
-        messages.scrollTop = messages.scrollHeight;
-      }
-    };
-
-    chatContainer.append(messages, inputMessage);
-
-    e.target.classList.contains('on')
-      ? lobby.appendChild(chatContainer)
-      : document.querySelector('#chat-container').remove();
-  });
-
-  main.appendChild(chat);
-
-  // ! TEST CHAT
 
   const copy = document.createElement('img');
   copy.src = 'src/assets/images/icons/menu/copy.svg';
@@ -96,9 +38,19 @@ const PlayersHub = (code, username, party) => {
     player++;
   }
 
-  playersHub.append(room, nickname, players);
+  const chat = document.createElement('img');
+  chat.setAttribute('role', 'button');
+  chat.className = 'chat-icon';
+  chat.src = 'src/assets/images/icons/menu/chat.svg';
 
-  Button('READY', 'ready-button', 'submit', playersHub);
+  chat.addEventListener('click', (e) => {
+    e.target.classList.toggle('on');
+    ChatBox(username, playersHub);
+  });
+
+  playersHub.append(room, nickname, players, chat);
+
+  Button('', 'ready-button', 'submit', lobby);
   let ready = document.querySelector('.ready-button');
   ready.addEventListener('click', () => toggleReadyState(ready, code, username));
 
@@ -106,14 +58,18 @@ const PlayersHub = (code, username, party) => {
   let cancel = document.querySelector('.cancel-lobby');
   cancel.addEventListener('click', () => {
     sendRequest('exitLobby', username, code);
-    closePlayersHub(playersHub, ready, cancel);
+    setTimeout(() => closePlayersHub(playersHub, ready, cancel), 100);
   });
 
   updateReadyState(party);
 
   ws.onmessage = function (event) {
     const current = JSON.parse(event.data);
-    updateReadyState(current);
+    if (current.tag === 'chat') {
+      chatMessage(current.name, current.message);
+    } else {
+      updateReadyState(current);
+    }
   };
 };
 
@@ -125,10 +81,8 @@ function toggleReadyState(ready, code, username) {
 
 function updateReadyState(data) {
   let players = document.querySelectorAll('.players img');
-  let length = data.players.length;
   data.players.forEach((player, index) => {
-    if (length === 0 || length > 3) return;
-    player && player.ready ? (players[index].style.opacity = '1') : (players[index].style.opacity = '');
+    player.ready ? (players[index].style.opacity = '0.9') : (players[index].style.opacity = '');
   });
 }
 
