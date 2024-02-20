829998453314c3774c16e92286f48f477f642d7f
JCBS <123596160+JCBalabuch@users.noreply.github.com>
2024-02-19 23:01:57 -0400

    Jcbs (#18)
    * FEAT modalContent and title; FIX modal
    
    * Delete Prettier-JSON-Config
    
    * Listeners MenuField
    
    * Add listeneres menuField

--- src/components/molecules/menuField/MenuField.js
+++ src/components/molecules/menuField/MenuField.js
@@ -1,23 +1,52 @@
-import { playSound } from '../../atoms/audio/Audio';
+import { Lobby } from '../../../pages/Lobby/Lobby';
+import Audio, { playSound } from '../../atoms/audio/Audio';
 import Menu from '../../atoms/menu/Menu';
+import createModal from '../../atoms/modal/Modal';
+import modalContent from '../../atoms/modalContent/ModalContent';
 import './MenuField.css';
 
-const hoverMenu = '../../../../src/assets/audio/sounds/main/Menu-hover.mp3';
+const hoverMenu = Audio('../../../../src/assets/audio/sounds/main/Menu-hover.mp3');
+const instructionsMenu = ['ESTO ES UNA PRUUEBA PARA EL MODAL DEL MENU', 'A VER SI FUNCIONA CORRECTAMENTE'];
+const creditsMenu = ['MANU', 'LUCA', 'JENI'];
 
 const menuField = (parent) => {
-  const menuField = document.createElement('div');
-  menuField.id = 'menu-field';
+  const intro = document.getElementById('intro');
 
-  Menu('START', 'start-button', menuField);
-  Menu('INSTRUCTIONS', 'instructions-button', menuField);
-  Menu('CREDITS', 'credits-button', menuField);
+  const menuField$ = document.createElement('div');
+  menuField$.id = 'menu-field';
 
-  const menuItems = document.querySelectorAll('menu-field');
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener('mouseenter', playSound(hoverMenu));
+  Menu('START', 'start-menu', menuField$);
+  Menu('INSTRUCTIONS', 'instructions-menu', menuField$);
+  Menu('CREDITS', 'credits-menu', menuField$);
+
+  parent.append(menuField$);
+
+  let menuIntro = document.querySelectorAll('[role="menu-select"]');
+
+  menuIntro.forEach((element) => {
+    element.addEventListener('mouseenter', () => {
+      playSound(hoverMenu);
+    });
+
+    if (element.classList.contains('start-menu')) {
+      element.addEventListener('click', () => {
+        app.innerHTML = '';
+        Lobby();
+      });
+    } else if (element.classList.contains('instructions-menu')) {
+      element.addEventListener('click', () => {
+        intro.innerHTML = '';
+        createModal('instructions', intro);
+        modalContent('instructions', instructionsMenu);
+      });
+    } else if (element.classList.contains('credits-menu')) {
+      element.addEventListener('click', () => {
+        intro.innerHTML = '';
+        createModal('credits', intro);
+        modalContent('credits', creditsMenu);
+      });
+    }
   });
-
-  parent.append(menuField);
 };
 
 export default menuField;
