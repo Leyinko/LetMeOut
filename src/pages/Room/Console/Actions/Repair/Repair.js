128fb47d835079703f2b766d6589f09437c9332c
leyinko <co.giro.luca@gmail.com>
2024-03-22 00:18:55 +0100

    Boh

--- src/pages/Room/Console/Actions/Repair/Repair.js
+++ src/pages/Room/Console/Actions/Repair/Repair.js
@@ -2,15 +2,15 @@ import MemoryPath from '../../../../games/memoryPath/memoryPath';
 import Smash from '../../../../games/smashThatTrash/Smash';
 
 const Repair = () => {
-  // First Game
-  MemoryPath();
-  // Smash();
-
   // Soundtrack Change
   let audio = document.querySelector('audio');
   audio.pause();
 
   setTimeout(() => {
+    // First Game
+    MemoryPath();
+    // Smash();
+
     audio.src = 'src/assets/audio/music/The-Trapper.mp3';
     audio.play();
   }, 1600);
